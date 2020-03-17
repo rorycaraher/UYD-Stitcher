101e0b37a230f5648909e32bb66afdaf3b0d8fe6
Rory Caraher <caraher.rory@gmail.com>
2020-03-17 18:47:55 +0000

    first commit

---
+++ uyd_stitcher.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.stitcher.com/podcast/uhh-yeah-dude")
+# print(r.text)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+# latest_episode = soup.select("#currentEpisode a.control")
+latest_episode = soup.find('section', {'id': 'currentEpisode'})
+episode_onclick = latest_episode.findChildren('a')[0]['onclick']
+onclick_split = episode_onclick.split(',')[2]
+episode_id = onclick_split.split('_')[1].replace("'","")
+latest_episode_link = "https://www.stitcher.com/podcast/uhh-yeah-dude/e/" + episode_id
+print(latest_episode_link)
